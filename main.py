652c1f745dc780e0857fa259915647b2703ff407
MattSona99 <67501051+MattSona99@users.noreply.github.com>
2024-12-17 11:17:45 +0100

    Aggiornamento tree_induction entropy/gini

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import os
 
+FOLDER_PATH = "C:\Users\matte\OneDrive\Desktop\Università 2\1 - Intelligenza Artificiale (Dragoni)\Stellar Classification\Classificazione Galattica"
 INDUCTION_FILE = "tree_induction_entropy.pl"
 
 def switchPath(filename):
@@ -30,11 +31,86 @@ def on_combobox_change():
     else: INDUCTION_FILE = "tree_induction_entropy.pl"
     print(f"Hai selezionato: {INDUCTION_FILE}")
 
+def format_value(value):
+    output = ""
+    if isinstance(value, list):
+        output = "[ " + ", ".join([format_value(val) for val in value]) + " ]"
+    elif isinstance(value, Functor) and value.arity == 2:
+        output = "{0}{1}{2}".format(value.args[0], value.name, value.args[1])
+    else:
+        output = "{}".format(value)
+    return output    
+
+def format_result(result):
+    result = list(result)
+    if len(result) == 0:
+        return "false."
+    if len(result) == 1 and len(result[0]) == 0:
+        return "true."
+    output = ""
+    for res in result:
+        tmpOutput = []
+        for var in res:
+            tmpOutput.append(var + " = " + format_value(res[var]))
+        output += ", ".join(tmpOutput) + " ;\n"
+    output = output[:-3] + " ."
+    return output
 
 def interroga():
     switchPath('tree_induction_entropy.pl')
     switchPath('tree_induction_gini.pl')
     prolog = Prolog()
+    #Determino i valori inseriti dall'utente
+    values = [entry.get() for entry in entry_widgets]
+    values[5] = str(float(values[5]) / 100 )#Normalizzo la percentuale
+    #Carico il file da consultare
+    prolog.consult(FOLDER_PATH+"Apprendimento_QSG/"+INDUCTION_FILE)
+    #Costruisco la query ricavandomi il tier
+    query = "["
+    for chiave, user_input in zip(attributi_dict.keys(), values):
+        if len(user_input) == 0: user_input = 0
+        tier_result = get_tier_for_value(chiave, attributi_dict[chiave], float(user_input))
+        print(f"Il tier corrispondente per "+chiave+" è: "+tier_result)
+        query = query+chiave+"="+tier_result+","
+    query = query[:-1]
+    query=query+"]"
+    print(query)
+
+    print("Inizio l'apprendimento dei quasar")
+    tempo_inizio=time.time()
+    answer = prolog.query("lancia_apprendi(QSO).")
+    print(format_result(answer))
+    tempo_fine=time.time()
+    tempo_totale = tempo_fine - tempo_inizio
+    print(f"Tempo totale di esecuzione: {tempo_totale} secondi")
+
+    print("Inizio l'apprendimento delle stelle")
+    tempo_inizio = time.time()
+    answer = prolog.query("lancia_apprendi(STAR).")
+    print(format_result(answer))
+    tempo_fine = time.time()
+    tempo_totale = tempo_fine - tempo_inizio
+    print(f"Tempo totale di esecuzione: {tempo_totale} secondi")
+
+    print("Inizio l'apprendimento delle galassie")
+    tempo_inizio = time.time()
+    answer = prolog.query("lancia_apprendi(GALAXY).")
+    print(format_result(answer))
+    tempo_fine = time.time()
+    tempo_totale = tempo_fine - tempo_inizio
+    print(f"Tempo totale di esecuzione: {tempo_totale} secondi")
+
+    print("Interrogo il programma")
+    tempo_inizio = time.time()
+    answer = prolog.query(("classifica_oggetto(" + query + ", Classe)."))
+    print(format_result(answer))
+    tempo_fine = time.time()
+    tempo_totale = tempo_fine - tempo_inizio
+    print(f"Tempo totale di esecuzione: {tempo_totale} secondi")
+
+
+
+
 
 root = tk.Tk()
 root.title("Apprendimento intelligente QSG")
