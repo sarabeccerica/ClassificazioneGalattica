614fc565804482bcf25c80f89bc2daa39ed32f21
MattSona99 <67501051+MattSona99@users.noreply.github.com>
2024-12-28 11:38:54 +0100

    Add files via upload

--- Apprendimento_QSG/attributi.pl
+++ Apprendimento_QSG/attributi.pl
@@ -1,6 +1,6 @@
-a(u, u_bin1, u_bin2, u_bin3, u_bin4, u_bin5, u_bin6,u_bin7, u_bin8,u_bin9, u_bin10).
-a(g, g_bin1, g_bin2, g_bin3, g_bin4, g_bin5, g_bin6,g_bin7, g_bin8,g_bin9, g_bin10).
-a(r, r_bin1, r_bin2, r_bin3, r_bin4, r_bin5, r_bin6,r_bin7, r_bin8,r_bin9, r_bin10).
-a(i, i_bin1, i_bin2, i_bin3, i_bin4, i_bin5, i_bin6,i_bin7, i_bin8,i_bin9, i_bin10).
-a(z, z_bin1, z_bin2, z_bin3, z_bin4, z_bin5, z_bin6,z_bin7, z_bin8,z_bin9, z_bin10).
-a(redshift_bin1,redshift_bin2, redshift_bin3).
+a(u, [bin1, bin2, bin3, bin4, bin5, bin6, bin7, bin8, bin9, bin10]).
+a(g, [bin1, bin2, bin3, bin4, bin5, bin6, bin7, bin8, bin9, bin10]).
+a(r, [bin1, bin2, bin3, bin4, bin5, bin6, bin7, bin8, bin9, bin10]).
+a(i, [bin1, bin2, bin3, bin4, bin5, bin6, bin7, bin8, bin9, bin10]).
+a(z, [bin1, bin2, bin3, bin4, bin5, bin6, bin7, bin8, bin9, bin10]).
+a(redshift, [bin1, bin2, bin3]).
