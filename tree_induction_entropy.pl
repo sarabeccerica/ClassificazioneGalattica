e0a990056da4d661d070285996249b310172878d
sarabeccerica <sarabeccerica@gmail.com>
2025-01-03 16:25:46 +0100

    modifica tree_induction_entropy.pl

--- tree_induction_entropy.pl
+++ tree_induction_entropy.pl
@@ -15,16 +15,27 @@
 % Con il predicato stampa_matrice_di_confusione e' possibile
 % visualizzare le prestazioni dell'albero indotto.
 
-% Cambiare percorso con quello della propria workspace
-file_output('C:/Users/matte/OneDrive/Documents/GitHub/ClassificazioneGalattica/albero.pl').
+file_output('./albero.pl').
 
 
 % Predicato per lanciare l'apprendimento
 lancia_apprendi(Classe) :-
     file_output(NomeFile),
-    tell(NomeFile),
-    apprendi(Classe),
+    write('File di output: '), writeln(NomeFile),
+    (tell(NomeFile) ->
+        write('File di output aperto: '), writeln(NomeFile);
+        writeln('Errore: impossibile aprire il file di output'), fail),
+    write('Debug: Apertura del file completata'), nl, % Debug
+    write('Sto per chiamare apprendi per la classe: '), writeln(Classe),
+    findall(e(C, O), e(C, O), Esempi),
+    write('Esempi trovati: '), writeln(Esempi),
+    (apprendi(Esempi, Classe, Descrizione) ->
+        write('Descrizione appresa: '), writeln(Descrizione);
+        writeln('Errore: apprendi ha fallito')),
     told.
+
+
+
 
 % Predicato per lanciare la classificazione di un oggetto
 classifica_oggetto(Oggetto,Classe) :-
@@ -52,28 +63,40 @@ apprendi(Classe) :-
 
 % apprendi(Esempi,Classe,Descrizione)
 % Descrizione copre esattamente gli esempi di Classe nella lista Esempi
-apprendi(Esempi,Classe,[]) :-
-    \+ member(e(Classe,_),Esempi). % non ci sono piu' esempi da coprire
-apprendi(Esempi,Classe,[Congie|Congi]) :-
-    apprendi_cong(Esempi,Classe,Congie),
-    rimuovi(Esempi,Congie,RestoEsempi), % rimuove gli esempi utilizzati
-    apprendi(RestoEsempi,Classe,Congi). % copre gli esempi restanti
+apprendi(Esempi, Classe, []) :-
+    write('Esempi disponibili: '), writeln(Esempi), % Debug
+    \+ member(e(Classe, _), Esempi),
+    write('Nessun esempio per la classe: '), writeln(Classe), !.
+
+apprendi(Esempi, Classe, [Congie|Congi]) :-
+    write('Apprendimento in corso per classe: '), writeln(Classe), % Debug
+    apprendi_cong(Esempi, Classe, Congie),
+    write('Congiunzione trovata: '), writeln(Congie), % Debug
+    rimuovi(Esempi, Congie, RestoEsempi),
+    write('Esempi rimanenti: '), writeln(RestoEsempi), % Debug
+    apprendi(RestoEsempi, Classe, Congi).
 
 % apprendi_cong(Esempi,Classe,Cong)
 % Cong e' una lista di coppie attributo-valore soddisfatti da alcuni
 % esempi di Classe e da nessun esempio di un'altra classe
-apprendi_cong(Esempi,Classe,[]) :-
-    \+ (member(e(ClasseX,_),Esempi), % non ci sono esempi di altre classi
-    ClasseX \== Classe),!.
-apprendi_cong(Esempi,Classe,[Cond|Conds]) :-
-    scegli_cond(Esempi,Classe,Cond), % sceglie attributo-valore
-    filtra(Esempi,[Cond],NuoviEsempi), % filtra esempi in base a Cond
-    apprendi_cong(NuoviEsempi,Classe,Conds).
+apprendi_cong(Esempi, Classe, []) :-
+    write('Verifica esempi per congiunzione: '), writeln(Esempi), % Debug
+    \+ (member(e(ClasseX, _), Esempi), ClasseX \== Classe), !.
 
-scegli_cond(Esempi,Classe,AttVal) :-
-    findall(AV/Punti,punti(Esempi,Classe,AV,Punti),AVs),
-    best(AVs,AttVal). % trova tutte le coppie Att-Val degli esempi di Classe
-                      % assegnandogli un punteggio e sceglie quella migliore
+apprendi_cong(Esempi, Classe, [Cond|Conds]) :-
+    write('Selezione condizione per: '), writeln(Classe), % Debug
+    scegli_cond(Esempi, Classe, Cond),
+    write('Condizione scelta: '), writeln(Cond), % Debug
+    filtra(Esempi, [Cond], NuoviEsempi),
+    write('Esempi filtrati: '), writeln(NuoviEsempi), % Debug
+    apprendi_cong(NuoviEsempi, Classe, Conds).
+
+
+scegli_cond(Esempi, Classe, AttVal) :-
+    findall(AV/Punti, punti(Esempi, Classe, AV, Punti), AVs),
+    write('Candidati trovati: '), writeln(AVs), % Debug
+    best(AVs, AttVal),
+    write('Migliore candidato: '), writeln(AttVal). % Debug
 
 punti(Esempi,Classe,AttVal,Punti) :-
     candidato(Esempi,Classe,AttVal), % un attributo-valore adatto
@@ -94,7 +117,7 @@ adatto(AttVal,Esempi,Classe) :-
     \+ soddisfa(OggX,[AttVal]),!. % esempio negativo che non matcha
 
 soddisfa(Oggetto,Congiunzione) :-
-    \+ (member(Att=Valx,Congiunzione), % soddisfa ha successo se non � vero che
+    \+ (member(Att=Valx,Congiunzione), % soddisfa ha successo se non è vero che
         member(Att=Valy,Oggetto),      % esiste all'interno di congiunzione un
         Valx \== Valy).                % valore Valx associato ad Att che sia
                                        % diverso dal valore Valy associato ad Att
@@ -102,7 +125,7 @@ soddisfa(Oggetto,Congiunzione) :-
 
 best([AttVal/_],AttVal).
 best([AV0/S0,AV1/S1|AVSlist],AttVal) :-
-    S1 > S0,!, % AV1 � meglio di AV0
+    S1 > S0,!, % AV1 è meglio di AV0
     best([AV1/S1|AVSlist],AttVal) % if S1 > S0
     ;
     best([AV0/S0|AVSlist],AttVal). % else if S0 > S1
@@ -143,6 +166,7 @@ induce_albero( Albero ) :-
 	mostra( Albero ),
 	assert(alb(Albero)).
 
+
 % induce_albero( +Attributi, +Esempi, -Albero):
 % l'Albero indotto dipende da questi tre casi:
 % (1) Albero = null: l'insieme degli esempi è vuoto
@@ -177,7 +201,7 @@ sceglie_attributo( Attributi, Esempi, MigliorAttributo )  :-
 	      [_/MigliorAttributo|_] ).
 
 % disuguaglianza(+Esempi, +Attributo, -Dis):
-% Dis � la disuguaglianza combinata dei sottoinsiemi degli esempi
+% Dis   la disuguaglianza combinata dei sottoinsiemi degli esempi
 % partizionati dai valori dell'Attributo
 disuguaglianza( Esempi, Attributo, Dis) :-
 	a( Attributo, AttVals),
